68da1c0a9c835d8638293056e51d139c6c7f1929
WdvOps <dvops@outlook.com>
2021-10-27 19:03:16 -0300

    Projeto 2 Level 2 Done

--- js/script.js
+++ js/script.js
@@ -6,3 +6,11 @@ function closeModal() {
     let element = document.querySelector(".modal-container");
     element.style.display = "none";
 }
+function openPhoto() {
+    let element = document.querySelector(".photo-galery");
+    element.style.display = "flex";
+}
+function closePhoto() {
+    let element = document.querySelector(".photo-galery");
+    element.style.display = "none";
+}
